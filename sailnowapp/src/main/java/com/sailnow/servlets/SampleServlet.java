17f766acf94de8c8978d081d30afa2fd6a723455
Yasinjama <yasinj6@gmail.com>
2016-04-20 22:55:21 -0500

    added test sample application SampleServlet to add user and Packages

--- sailnowapp/src/main/java/com/sailnow/servlets/SampleServlet.java
+++ sailnowapp/src/main/java/com/sailnow/servlets/SampleServlet.java
@@ -2,6 +2,7 @@ package com.sailnow.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -42,20 +43,36 @@ public class SampleServlet extends HttpServlet {
 		{
 
 			String name = request.getParameter("name");
-			String description = request.getParameter("description");
+			PackageService pkservice = mgr.getPackageService();
+			PackageModel pkmodel = pkservice.findPackage(name);
+			if(pkmodel!=null)
+			{
+				writer.println("Package "+name+" already exist");
+				return;
+			}			String description = request.getParameter("description");
 			String duration = request.getParameter("duration");
 			double price = Double.parseDouble(request.getParameter("price"));
 			
 			String userid = request.getParameter("userid");
-			PackageService pkservice = mgr.getPackageService();
+
 			UserModel user = mgr.getUserService().findUser(userid);
+			if(user == null)
+			{
+				writer.println("User "+userid+" not found");
+				return;
+			}
+
 			pkservice.createPackage(new PackageModel(name,description,duration,price,user));
 			writer.print("Package "+name+" successfully created");
 		}else if(action.equals("findPackage"))
 		{
 			String name = request.getParameter("name");
 			PackageModel pkmodel = mgr.getPackageService().findPackage(name);
-			
+			if(pkmodel == null)
+			{
+				writer.print("Unable to find Package "+name);
+				return;
+			}
 			String output = outPutJson(pkmodel);
 			
 			response.setContentType("application/json");
@@ -63,10 +80,21 @@ public class SampleServlet extends HttpServlet {
 		}else if(action.equals("createUser"))
 		{
 			String email = request.getParameter("userid");
-			
+			UserModel user = mgr.getUserService().findUser(email);
+			if(user != null)
+			{
+				writer.println("User "+email+" already exist");
+				return;
+			}
 			mgr.getUserService().createUser(email);
-			
-			writer.println("Successfully created "+mgr.getUserService().findUser(email).getEmail());
+			writer.write("Successfully created "+mgr.getUserService().findUser(email).getEmail());
+//			writer.println("Successfully created "+mgr.getUserService().findUser(email).getEmail());
+		}else if(action.equals("listAllPackages"))
+		{
+			JSONObject json = new JSONObject();
+			List<PackageModel> list = mgr.getPackageService().listAllPackages();
+			json.put("Packages", list);
+			writer.println(json.toString());
 		}
 			
 		
@@ -74,8 +102,11 @@ public class SampleServlet extends HttpServlet {
 
 	private String outPutJson(PackageModel pkmodel) {
 		JSONObject json = new JSONObject();
-		json.put("Package", pkmodel);
-		
+		json.put("name", pkmodel.getName());
+		json.put("description", pkmodel.getDescription());
+		json.put("duration", pkmodel.getDuration());
+		json.put("price", pkmodel.getPrice());
+		json.put("userid", pkmodel.getUsers().getEmail());
 		return json.toString();
 	}
 
