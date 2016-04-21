b560bb9a1661c2b0334471d810cd4770fccc2a1d
dangolanup <dangol.anup@gmail.com>
2016-04-21 13:25:12 -0500

    Added js files

---
+++ sailnowapp/src/main/webapp/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready( function() {
+  $('.cookies-used').fadeIn();
+  setEvents();
+});
+
+setEvents = function() {
+  $('.black-complete-background').click( function() {
+    $('.password-reset-div').fadeOut();
+  });
+}
+
+showPasswordForgotten = function() {
+  $('.password-reset-div').fadeIn();
+}
+
+closePF = function() {
+   $('.password-reset-div').fadeOut();
+}
